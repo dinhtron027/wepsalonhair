c0ba50cd5c60586ae5b1b44ffb58f7fe0b093c95
DINH TRONG <cutrong236@gmail.com>
2026-06-21 20:33:26 +0000

    fix: pass VITE_GOOGLE_CLIENT_ID build argument to frontend Docker image

--- backend/config/env.js
+++ backend/config/env.js
@@ -69,7 +69,8 @@ const envSchema = Joi.object({
   DEFAULT_CUSTOMER_PHONE: Joi.string().default('0900000002'),
   DEFAULT_CUSTOMER_PASSWORD: Joi.string().min(6).default('Customer@123456'),
   GOOGLE_CLIENT_ID: Joi.string().allow('').optional(),
-  GOOGLE_CLIENT_SECRET: Joi.string().allow('').optional()
+  GOOGLE_CLIENT_SECRET: Joi.string().allow('').optional(),
+  GOOGLE_CALLBACK_URL: Joi.string().allow('').optional()
 })
   .unknown()
   .prefs({ abortEarly: false });
